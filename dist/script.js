6f6e565d1ee5585c8fdb555e20ee7e031e521808
trisssh <samillanotrisha768@gmail.com>
2023-07-19 15:38:41 +0800

    update july-19

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,8 @@
 const toggle = document.querySelector('.toggle-menu');
-const menu = document.querySelector('nav ul');
+const menu = document.querySelector('.header__nav');
 
 toggle.addEventListener('click', () => {
     toggle.classList.toggle('open');
     menu.classList.toggle('open');
 })
+
